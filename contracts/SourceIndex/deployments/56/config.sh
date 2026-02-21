bbfd63bd6bd31ed1a5d7d4edcfd83aef094d160d
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-02-21 20:17:03 +0000

    add binance chain deployment

---
+++ contracts/SourceIndex/deployments/56/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.28"
+)
+evm_version=(
+  ["1.0"]="cancun"
+)
+contract_address=(
+  ["1.0"]="0xd66866aF7bc1F78a2855243DF9DCE9A8a8F7EdFf"
+)
