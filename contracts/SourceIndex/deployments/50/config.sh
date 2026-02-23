7ded7406b6ae8386f0168c67067c648be46da784
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-02-23 13:22:57 +0000

    add chain 50 deployment

---
+++ contracts/SourceIndex/deployments/50/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.24"
+)
+evm_version=(
+  ["1.0"]="paris"
+)
+contract_address=(
+  ["1.0"]="0x1fBE19F6717eFF433c6f379571070da74EcfE418"
+)
